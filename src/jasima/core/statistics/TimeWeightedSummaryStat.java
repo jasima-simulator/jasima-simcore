a1390fb164fb1a30f24ca5a7ce3eea8bdbcd1739
THildebrandt@gmail.com <THildebrandt@gmail.com@76137c3f-ef0f-7569-c771-fddd727a4a73>
2014-06-20 12:56:56 +0000

    added documentation and extended error checking
    git-svn-id: http://jasima.googlecode.com/svn/trunk/jasima@159 76137c3f-ef0f-7569-c771-fddd727a4a73

--- src/jasima/core/statistics/TimeWeightedSummaryStat.java
+++ src/jasima/core/statistics/TimeWeightedSummaryStat.java
@@ -21,21 +21,70 @@
 package jasima.core.statistics;
 
 /**
- * Summary stat which weights values according to the time.
+ * A {@link SummaryStat} which weights values according to the time. As result
+ * statistics the values returned by {@link #mean()}, {@link #min()}, and
+ * {@link #max()} have defined values.
  * 
  * @author Torsten Hildebrandt <hil@biba.uni-bremen.de>
- * @version "$Id$"
+ * @version 
+ *          "$Id$"
  */
 public class TimeWeightedSummaryStat extends SummaryStat {
+
 	private static final long serialVersionUID = -2192851354158356984L;
 
 	private double lastTime = 0.0d;
 
+	/**
+	 * Constructs a new instance with {@code lastTime} and {@code lastValue}
+	 * initialized with 0.
+	 */
+	public TimeWeightedSummaryStat() {
+		this(0.0d, 0.0d);
+	}
+
+	/**
+	 * Constructs a new instance initializing {@code lastTime} with the given
+	 * parameter {@code initialTime}.
+	 */
+	public TimeWeightedSummaryStat(double initialTime, double initialValue) {
+		super();
+		lastTime = initialTime;
+		lastValue = initialValue;
+	}
+
+	/**
+	 * Adds a new {@code value}, weighted by the difference between {@code time}
+	 * and {@link #lastTime()}. {@code time} is then saved for the next
+	 * invocation of this method.
+	 */
 	@Override
 	public void value(double value, double time) {
-		super.value(lastValue, time - lastTime);
+		if (time < lastTime())
+			throw new IllegalArgumentException(String.format(
+					"negative time span (lastTime=%f,time=%f).", lastTime(),
+					time));
+		super.value(lastValue, time - lastTime());
 		lastTime = time;
 		lastValue = value;
+	}
+
+	/**
+	 * Don't use this method as it doesn't make sense for a
+	 * {@code TimeWeightedSummaryStat}. Raises an
+	 * {@link UnsupportedOperationException} when called.
+	 */
+	@Override
+	public void value(double v) {
+		throw new UnsupportedOperationException(
+				"Use method TimeWeightedSummaryStat.value(double,double) instead.");
+	}
+
+	/**
+	 * Returns the current value of the attribute {@code lastTime}.
+	 */
+	public double lastTime() {
+		return lastTime;
 	}
 
 }
