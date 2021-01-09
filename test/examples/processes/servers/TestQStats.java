6ec893dbd10e908fe1a1bec644a25f9d6e158cc1
Torsten Hildebrandt <Torsten.Hildebrandt@simplan.de>
2021-01-09 12:43:17 +0100

    enh: qStats test

--- test/examples/processes/servers/TestQStats.java
+++ test/examples/processes/servers/TestQStats.java
@@ -20,12 +20,25 @@ public class TestQStats {
 		Map<String, Object> res = SimContext.of(sim -> {
 			Q<String> q = new Q<String>();
 			QStatCollector<String> qColl = new QStatCollector<>(q, sim);
+		});
+		ConsolePrinter.printResults(null, res);
+
+		TimeWeightedSummaryStat stats = (TimeWeightedSummaryStat) res.get("qStats");
+		checkCountMeanMinMaxWeightSum(stats, 2, 0.0, 0.0, 0.0, 0.0);
+		assertEquals("last value", 0.0, stats.lastValue(), 1e-6);
+	}
+
+	@Test
+	public void testNoopWithDelay() throws MightBlock {
+		Map<String, Object> res = SimContext.of(sim -> {
+			Q<String> q = new Q<String>();
+			QStatCollector<String> qColl = new QStatCollector<>(q, sim);
 			waitFor(1.0);
 		});
 		ConsolePrinter.printResults(null, res);
 
 		TimeWeightedSummaryStat stats = (TimeWeightedSummaryStat) res.get("qStats");
-		checkMinMaxMeanCount(stats, 2, 0.0, 0.0, 0.0);
+		checkCountMeanMinMaxWeightSum(stats, 2, 0.0, 0.0, 0.0, 1.0);
 		assertEquals("last value", 0.0, stats.lastValue(), 1e-6);
 	}
 
@@ -39,12 +52,12 @@ public class TestQStats {
 			waitFor(100.0);
 		});
 		TimeWeightedSummaryStat stats = (TimeWeightedSummaryStat) res.get("qStats");
-		
-		ConsolePrinter.printResults(null, res);
-		
-		checkMinMaxMeanCount(stats, 2+1, (0.0*100.0+1.0*100.0)/200.0, 0.0, 1.0);
 
-		assertEquals("last value", 0.0, stats.lastValue(), 1e-6);
+		ConsolePrinter.printResults(null, res);
+
+		checkCountMeanMinMaxWeightSum(stats, 2 + 1, (0.0 * 100.0 + 1.0 * 100.0) / 200.0, 0.0, 1.0, 200.0);
+
+		assertEquals("last value", 1.0, stats.lastValue(), 1e-6);
 	}
 
 	@Test
@@ -58,12 +71,12 @@ public class TestQStats {
 			waitFor(100.0);
 		});
 		TimeWeightedSummaryStat stats = (TimeWeightedSummaryStat) res.get("qStats");
-		
-		ConsolePrinter.printResults(null, res);
-		
-		checkMinMaxMeanCount(stats, 2+1, (0.0*100.0+1.0*100.0)/200.0, 0.0, 1.0);
 
-		assertEquals("last value", 0.0, stats.lastValue(), 1e-6);
+		ConsolePrinter.printResults(null, res);
+
+		checkCountMeanMinMaxWeightSum(stats, 2 + 1, (0.0 * 100.0 + 1.0 * 100.0) / 200.0, 0.0, 1.0, 200.0);
+
+		assertEquals("last value", 1.0, stats.lastValue(), 1e-6);
 	}
 
 	@Test
@@ -76,10 +89,10 @@ public class TestQStats {
 			waitFor(100.0);
 		});
 		TimeWeightedSummaryStat stats = (TimeWeightedSummaryStat) res.get("qStats");
-		
+
 		ConsolePrinter.printResults(null, res);
-		
-		checkMinMaxMeanCount(stats, 2, (1.0*100.0)/100.0, 1.0, 1.0);
+
+		checkCountMeanMinMaxWeightSum(stats, 2, (1.0 * 100.0) / 100.0, 1.0, 1.0, 100.0);
 
 		assertEquals("last value", 1.0, stats.lastValue(), 1e-6);
 	}
@@ -98,19 +111,21 @@ public class TestQStats {
 			q.take();
 		});
 		TimeWeightedSummaryStat stats = (TimeWeightedSummaryStat) res.get("qStats");
-		
+
 		ConsolePrinter.printResults(null, res);
-		
-		checkMinMaxMeanCount(stats, 3+2, (0.0*1.0+1.0*2.0+2.0*1.0)/4.0, 0.0, 2.0);
+
+		checkCountMeanMinMaxWeightSum(stats, 3 + 2, (0.0 * 1.0 + 1.0 * 2.0 + 2.0 * 1.0) / 4.0, 0.0, 2.0, 4.0);
 
 		assertEquals("last value", 1.0, stats.lastValue(), 1e-6);
 	}
 
-	private static void checkMinMaxMeanCount(SummaryStat stats, int numObs, double mean, double min, double max) {
+	private static void checkCountMeanMinMaxWeightSum(SummaryStat stats, int numObs, double mean, double min,
+			double max, double weightSum) {
 		assertEquals("mean", mean, stats.mean(), 1e-6);
 		assertEquals("min", min, stats.min(), 1e-6);
 		assertEquals("max", max, stats.max(), 1e-6);
 		assertEquals("numObs", numObs, stats.numObs());
+		assertEquals("weightSum", weightSum, stats.weightSum(), 1e-6);
 	}
 
 }
